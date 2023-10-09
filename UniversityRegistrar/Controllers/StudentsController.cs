da8cd09a6c0edd0c6a78ab39604cba9196c47c5c
Joey Palchak <joey.palchak@gmail.com>
2023-10-09 16:34:39 -0700

    add Index, Create, WIP Details for Students
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using UniversityRegistrar.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +18,30 @@ namespace UniversityRegistrar.Controllers
 
     public ActionResult Index()
     {
+      List<Student> model = _db.Students.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StudentId = new SelectList(_db.Students, "StudentId", "Name");
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Student thisStudent = _db.Students
+                                .Include(student => student.JoinEntities)
+                                .ThenInclude(student => student.Course)
+                                .FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
     }
   }
 }
