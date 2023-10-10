23505eb3f59f39898c2773df55b9cfb0b36c5621
Joey Palchak <joey.palchak@gmail.com>
2023-10-10 10:37:36 -0700

    create AddStudent & AddCourse views + routes
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- UniversityRegistrar/Controllers/CoursesController.cs
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using UniversityRegistrar.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace UniversityRegistrar.Controllers
 {
@@ -20,10 +21,12 @@ namespace UniversityRegistrar.Controllers
       List<Course> model = _db.Courses.ToList();
       return View(model);
     }
+
     public ActionResult Create()
     {
     return View();
     }
+
     [HttpPost]
     public ActionResult Create(Course course)
     {
@@ -31,6 +34,7 @@ namespace UniversityRegistrar.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
     public ActionResult Details(int id)
     {
       Course thisCourse = _db.Courses
@@ -38,6 +42,28 @@ namespace UniversityRegistrar.Controllers
                               .ThenInclude(course => course.Student)
                               .FirstOrDefault(course => course.CourseId == id);
       return View(thisCourse);
+    }
+
+    public ActionResult AddStudent(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      ViewBag.StudentId = new SelectList(_db.Students, "StudentId", "Name");
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult AddStudent(Course course, int studentId)
+    { 
+      #nullable enable 
+      Enrollment? joinEntity = _db.Enrollments.FirstOrDefault(join => (join.StudentId == studentId && join.CourseId == course.CourseId));
+      #nullable disable
+      
+      if (joinEntity == null && studentId != 0)
+      {
+        _db.Enrollments.Add(new Enrollment() { StudentId = studentId, CourseId = course.CourseId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = course.CourseId });
     }
   }
 }
--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -43,5 +43,31 @@ namespace UniversityRegistrar.Controllers
                                 .FirstOrDefault(student => student.StudentId == id);
       return View(thisStudent);
     }
+    
+    public ActionResult AddCourse(int id)
+    {
+      Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult AddCourse(Student student, int courseId)
+    { // Check the table for our many-to-many relationship: does this student exist in the table, and is this student ALREADY enrolled in this course?
+      // If this student is not enrolled in this course already, this relationship doesn't exist so this Enrollment object will be NULL
+      // If this student IS already enrolled in this course, this Enrollment object will be populated.
+      #nullable enable 
+      Enrollment? joinEntity = _db.Enrollments.FirstOrDefault(join => (join.CourseId == courseId && join.StudentId == student.StudentId));
+      #nullable disable
+      // So we check here to see if this relationship already exists, and if this student exists
+      // If the relationship DOES NOT exist, and the Student is real, create this new relationship!
+      // Otherwise, if this DOES exist already, don't do anything -> redirect to the Course detail page only
+      if (joinEntity == null && courseId != 0)
+      {
+        _db.Enrollments.Add(new Enrollment() { StudentId = student.StudentId, CourseId = courseId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = student.StudentId });
+    }
   }
 }
